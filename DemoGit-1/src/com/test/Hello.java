6dd59515480d8f1b97c42684e820b498b6bf820f
samadhan2308 <118296177+samadhan2308@users.noreply.github.com>
2022-12-16 11:00:35 +0530

    Update Hello.java

--- DemoGit-1/src/com/test/Hello.java
+++ DemoGit-1/src/com/test/Hello.java
@@ -3,7 +3,10 @@ package com.test;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub, this is my first commit...");
+		
+		System.out.println(" First Change By Samadhan");		
+		System.out.println("Hello GitHub, this is my first change...");
+		
 	}
 	
 }
