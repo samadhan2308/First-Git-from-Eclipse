a63b49474cefeeda90f8c6dc39c1034911eaa547
DELL <DELL@DESKTOP-TCOOR0K>
2022-12-16 11:09:25 +0530

    second change

--- DemoGit-1/src/com/test/Hello.java
+++ DemoGit-1/src/com/test/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(" First Change By Samadhan");		
-		System.out.println("Hello GitHub, this is my first change...");
+		System.out.println(" Second Change By Samadhan");		
+		System.out.println("Hello GitHub, this is my second commit...");
 		
 	}
 	
